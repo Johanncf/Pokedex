b3974497d3f174fa4878b2209a6af3cdf2e2f64e
Johann <johanncf@metalmat.ufrj.br>
2021-12-13 22:16:16 -0300

    Comentei linhas bugadas

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import './App.css';
 import Home from './pages/Home';
-import PokeCard from './components/PokeCard';
-import PokemonSearchForm from './components/PokemonSearch';
-import { getPokemons } from './dependencies/apiCall';
-import { useEffect, useState } from 'react'
+// import { getPokemons } from './dependencies/apiCall';
+// import { useEffect, useState } from 'react'
 function App() {
 
-  const [pokemonName, setPokemonName] = useState('')
-  const [pokemons, setPokemons] = useState([])
+  // const [pokemonName, setPokemonName] = useState('')
+  // const [pokemons, setPokemons] = useState([])
 
-  function getPokemon() {
+  // function getPokemon() {
 
-    const input = document.getElementById('input')
-    setPokemonName(input.value)
-    console.log(input.value)
+  //   const input = document.getElementById('input')
+  //   setPokemonName(input.value)
+  //   console.log(input.value)
 
-  }
+  // }
 
-  useEffect(() => {
-    getPokemons("?limit=2000")
-    .then(response => {
-      setPokemons(response.results)
-    })
-  })
+  // useEffect(() => {
+  //   getPokemons("?limit=2000")
+  //     .then(response => {
+  //       setPokemons(response.results)
+  //     })
+  // })
 
   return (
     <div className="container">
       <h1 className="title">Pokédex</h1>
 
-      <PokemonSearchForm sourcePokemonList={pokemons} setPokemons={setPokemons} listSize={pokemons.length} />
-
+      {/* <PokemonSearchForm sourcePokemonList={pokemons} setPokemons={setPokemons} listSize={pokemons.length} /> */}
+      <form className="poke-form">
+        <input id="input" type="text" placeholder="Quem é esse pokemón?"/>
+      </form>
 
       <div className="cards-container" >
 
